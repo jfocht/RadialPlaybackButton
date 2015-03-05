c71d91c3c71c0f041498413b5d9d1215cc8132b1
Jordan Focht <jfocht@gmail.com>
2015-03-05 11:17:36 -0600

    Initial Commit

---
+++ RadialPlayback/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RadialPlayback
+//
+//  Created by Jordan Focht on 3/5/15.
+//  Copyright (c) 2015 Jordan Focht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
